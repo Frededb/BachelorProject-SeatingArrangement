62ac02e69b6d970e1128ecf56f138955e29ea928
AugustBugge <aubu@itu.dk>
2026-02-04 16:19:34 +0100

    Started influencelist

---
+++ InfluenceListGreedy/InfluenceListGreedy.py
@@ -0,0 +1,52 @@
+from Utils.ValueCalc import calcTable
+import math
+
+arrangement = []
+
+def newArrangement(arrangementSize):
+    global arrangement
+    #sets up the arrangement 2d array based on input size
+    arrangement = [[None] * 8 for _ in range(math.ceil(arrangementSize/8))]
+
+#only takes preferences and avoidances into account. Returns a sorted list of people from most to least influential
+def makeInfluenceList(list):
+    d = dict.fromkeys(list, 0)
+    for personA in list:
+        for personB in personA.preferences:
+            d[personA] = d[personA] + 10
+            d[personB] = d[personB] + 10
+        for personB in personA.avoidances:
+            d[personA] = d[personA] + 10
+            d[personB] = d[personB] + 10
+    return sorted(d, key=d.get,reverse=True)
+
+
+def influenceListGreedy(input):
+    newArrangement(len(input))
+    influenceList = makeInfluenceList(input)
+    for person in influenceList:
+        placeGreedy(person)
+    return arrangement
+
+
+def placeGreedy(person):
+    global arrangement
+    bestImprovement = -math.inf
+    bestPlacement = (-1,-1)
+    for i in range(len(arrangement)):
+        preValue = calcTable(arrangement[i])[0]
+        for j in range(len(arrangement[i])):
+            #if the spot is already taken, skip
+            if arrangement[i][j] != None:
+                continue
+            arrangement[i][j] = person
+            postValue = calcTable(arrangement[i])[0]
+            #here we find the best improvement to a single table, which should give the best improvement overall
+            if postValue - preValue > bestImprovement:
+                bestImprovement = postValue - preValue
+                bestPlacement = (i,j)
+            arrangement[i][j] = 0
+    arrangement[bestPlacement[0]][bestPlacement[1]] = person
+
+
+
