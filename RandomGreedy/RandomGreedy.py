e6ee2f7df732c9dc0cbed865f80e9e3de82e84ee
AugustBugge <aubu@itu.dk>
2026-02-04 15:34:58 +0100

    Fixed random greedy

--- RandomGreedy/RandomGreedy.py
+++ RandomGreedy/RandomGreedy.py
@@ -1,32 +1,34 @@
 from Utils.ValueCalc import calcTable
 import math
+import random
 
 arrangement = []
 
 def newArrangement(arrangementSize):
     global arrangement
-    arrangement = [[0,0,0,0,0,0,0,0]]*math.ceil(arrangementSize/8)
+    arrangement = [[0] * 8 for _ in range(math.ceil(arrangementSize/8))]
 
 def randomGreedy(input):
-    newArrangement(len(input)/8)
-    import random
+    newArrangement(len(input))
     for _ in range(len(input)):
-        number = random.random(len(input))
-        placeGreedy(input[number])
+        number = random.randint(0,len(input)-1)
+        person = input.pop(number)
+        placeGreedy(person)
     return arrangement
 
 
 def placeGreedy(person):
     global arrangement
-    bestImprovement = -1000
+    bestImprovement = -math.inf
     bestPlacement = (-1,-1)
     for i in range(len(arrangement)):
+        preValue = calcTable(arrangement[i])
         for j in range(len(arrangement[i])):
-            if(arrangement[i][j] != 0):
+            if arrangement[i][j] != 0:
                 continue
-            preValue = calcTable(arrangement[i])
             arrangement[i][j] = person
             postValue = calcTable(arrangement[i])
+            # print(f"preValue:  {preValue} postValue:  {postValue} improvement:  {(postValue - preValue)} bestImprovement: {bestImprovement}")
             if postValue - preValue > bestImprovement:
                 bestImprovement = postValue - preValue
                 bestPlacement = (i,j)
