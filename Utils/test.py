ac9ba92f16ae2d2b4598f06df3d68b53a6ac19cc
AugustBugge <aubu@itu.dk>
2026-02-04 12:12:33 +0100

    bug fixes

--- Utils/test.py
+++ Utils/test.py
@@ -4,4 +4,4 @@ import printer
 
 table = reader.readjson("../reader/input1.json")
 print("lida: ", ValueCalc.calcPerson(table, 0))
-print("table: ", ValueCalc.calcTable(table))
+#print("table: ", ValueCalc.calcTable(table))
