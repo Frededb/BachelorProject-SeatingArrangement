1fe540d00110da1891b34390bdb428a10755769c
AugustBugge <aubu@itu.dk>
2026-02-04 09:22:48 +0100

    Finished RandomGreedy? no test

--- RandomGreedy/RandomGreedy.py
+++ RandomGreedy/RandomGreedy.py
@@ -1,21 +1,36 @@
 from Utils.ValueCalc import calcTable
+import math
 
 arrangement = []
 
-def setArrangement(arrangementSize):
+def newArrangement(arrangementSize):
     global arrangement
-    arrangement = [[0,0,0,0,0,0,0,0]]*arrangementSize
+    arrangement = [[0,0,0,0,0,0,0,0]]*math.ceil(arrangementSize/8)
 def randomGreedy(input):
+    newArrangement(len(input)/8)
     import random
-    random.random(len(input))
+    for _ in range(len(input)):
+        number = random.random(len(input))
+        placeGreedy(input[number])
+    return arrangement
+
+
 def placeGreedy(person):
     global arrangement
-    bestImprovement
+    bestImprovement = -1000
+    bestPlacement = (-1,-1)
     for i in range(len(arrangement)):
         for j in range(len(arrangement[i])):
             if(arrangement[i][j] != 0):
+                continue
             preValue = calcTable(arrangement[i])
             arrangement[i][j] = person
             postValue = calcTable(arrangement[i])
+            if postValue - preValue > bestImprovement:
+                bestImprovement = postValue - preValue
+                bestPlacement = (i,j)
+            arrangement[i][j] = 0
+    arrangement[bestPlacement[0]][bestPlacement[1]] = person
+
 
 
