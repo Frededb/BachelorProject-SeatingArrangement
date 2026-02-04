552efbc46984548b98558acdd16870e2e95a28a4
AugustBugge <aubu@itu.dk>
2026-02-04 17:16:23 +0100

    added tests

--- Utils/test.py
+++ Utils/test.py
@@ -7,6 +7,7 @@ from itertools import permutations
 
 input1 = reader.readjson("../reader/input1.json")
 input2 = reader.readjson("../reader/input2.json")
+input3 = reader.readjson("../reader/input3.json")
 
 def testcalcPerson():
     print("lida: ", ValueCalc.calcPerson(input1, 0))
@@ -19,10 +20,23 @@ def testcalcTable2():
 def testbruteForce():
     value = bruteForce(input1)
     print (value)
+    {
 
+        "name": "D1D1",
+        "studyprogram": "ds",
+        "year": "2023",
+        "preferences": ["D2D2"],
+        "avoidances": []
+    },
 def testRandomGreedy():
     from RandomGreedy.RandomGreedy import randomGreedy
     value = randomGreedy(input1)
     print (value)
 
-testRandomGreedy()
+def testInfluenceListGreedy():
+    from InfluenceListGreedy.InfluenceListGreedy import influenceListGreedy
+    value = influenceListGreedy(input3)
+    print (value)
+
+#testRandomGreedy()
+testInfluenceListGreedy()
