26bba0d10e682e3921ab87bc9901afbd998b9d53
Nicklas <nsio@itu.dk>
2026-02-04 13:08:07 +0100

    beautiful print

--- Utils/test.py
+++ Utils/test.py
@@ -3,5 +3,7 @@ import ValueCalc
 import printer
 
 table = reader.readjson("../reader/input1.json")
-print("lida: ", ValueCalc.calcPerson(table, 0))
-print("table: ", ValueCalc.calcTable(table))
+# print("lida: ", ValueCalc.calcPerson(table, 0))
+# print("table: ", ValueCalc.calcTable(table))
+# print(ValueCalc.calcArrangement([table]))
+printer.printArrangementWithValues([table, table])
