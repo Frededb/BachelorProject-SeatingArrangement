01b0969134307adf1066d4e83713a85c35f6447a
AugustBugge <aubu@itu.dk>
2026-02-04 15:35:11 +0100

    Fixed bruteforce

--- BruteForcePy/BruteForce.py
+++ BruteForcePy/BruteForce.py
@@ -1,27 +1,32 @@
 from Utils.ValueCalc import calcTable, calcArrangement
-from itertools import permutations
+from itertools import islice, permutations
 import math
 
 arrangement = []
 
 def newArrangement(arrangementSize):
     global arrangement
-    arrangement = [[0,0,0,0,0,0,0,0]]*math.ceil(arrangementSize/8)
-
-def bruteForce(input):
-    newArrangement(len(input)/8)
-    all_perms = list(permutations(input))
-    bestValue = -math.inf
-    bestArrangement = -1
-    for perm in all_perms:
-        permValue = makeArrangement(perm)
-        if(permValue) > bestValue:
-            bestValue = permValue
-            bestArrangement = arrangement
-    return bestArrangement
+    arrangement = [[0] * 8 for _ in range(math.ceil(arrangementSize/8))]
 
 def makeArrangement(perm):
     global arrangement
-    for i in range(perm):
+    for i in range(len(perm)):
         arrangement[i//8][i%8] = perm[i]
     return calcArrangement(arrangement)
+
+def bruteForce(input):
+    newArrangement(len(input))
+    #here I will generate all permutations of input
+    all_perms = permutations(input)
+    bestValue = -math.inf
+    bestArrangement = -1
+
+    #we go through each and see if they are better than the privius best
+    for perm in all_perms:
+        permValue = makeArrangement(perm)
+
+        if(permValue) > bestValue:
+            bestValue = permValue
+            bestArrangement = [row[:] for row in arrangement]
+    return bestArrangement
+
