6a80e588671c0ed381de17233f4d9dd6b741f4f6
AugustBugge <aubu@itu.dk>
2026-02-05 10:43:10 +0100

    finished it

--- Algorithms/DefaultPlacement.py
+++ Algorithms/DefaultPlacement.py
@@ -1,5 +1,8 @@
-def makeArrangement(input):
-    global arrangement
+import math
+
+
+def defaultPlacement(input):
+    arrangement = [[None] * 8 for _ in range(math.ceil(len(input) / 8))]
     for i in range(len(input)):
         arrangement[i//8][i%8] = input[i]
     return arrangement
