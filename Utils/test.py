41ce37cfda250ef258a3eec4a8d8968224c34aca
AugustBugge <aubu@itu.dk>
2026-02-06 09:56:18 +0100

    Added tests

--- Utils/test.py
+++ Utils/test.py
@@ -1,8 +1,6 @@
 import sys
 import os
 
-from Algorithms.switch import switchRandom
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import reader
@@ -37,46 +35,49 @@ def testInfluenceListGreedy(input = input1Table):
     value = influenceListGreedy(input)
     print("InfluenceListGreedy: ", calcArrangement(value)[0], value, calcArrangement(value))
 
-testInfluenceListGreedy(input7People)
-
 def testDefaultPlacement(input = input1Table):
     from Algorithms.DefaultPlacement import defaultPlacement
     default = defaultPlacement(input)
     print("DefaultPlacement: ", calcArrangement(default)[0], default, calcArrangement(default))
-
-def testSwitcher(input = input1Table):
-    from Algorithms.DefaultPlacement import defaultPlacement
-    default = defaultPlacement(input)
-    switchRandom(default)
-    print("After switch:")
-    printer.printArrangementWithValues(default)
 
 def testRandom(input = input1Table):
     from Algorithms.Random import randomArrangement
     randomArrangement = randomArrangement(input)
     print("Random: ", calcArrangement(randomArrangement)[0], randomArrangement, calcArrangement(randomArrangement))
 
-def testRepeatedRandom(input = input1Table):
+def testRepeatedRandom(input = input1Table, N = 1000000):
     from Algorithms.RepeatedRandom import repeatedRandom
-    randomArrangement = repeatedRandom(100, input)
+    randomArrangement = repeatedRandom(10000, input)
     print("RepeatedRandom: ", calcArrangement(randomArrangement)[0], randomArrangement, calcArrangement(randomArrangement))
 
-def testLinearSwitch(input = input1Table):
+def testLinearSwitch(input = input1Table, N = 10):
     from Algorithms.LinearSwitch import LinearSwitch
+    from Algorithms.Random import randomArrangement
     from Algorithms.DefaultPlacement import defaultPlacement
     arrangement = defaultPlacement(input)
-    LinearSwitch(arrangement)
-    printer.printArrangementWithValues(arrangement)
+    optimizedArrangement = LinearSwitch(arrangement, N)
+    print("LinearSwitch: ", calcArrangement(optimizedArrangement)[0], optimizedArrangement, calcArrangement(optimizedArrangement))
 
-def testOpt(input = input1Table):
+def testLinearSwitchRandom(input = input1Table, N = 10):
+    from Algorithms.LinearSwitch import LinearSwitch
+    from Algorithms.Random import randomArrangement
+    arrangement = randomArrangement(input)
+    optimizedArrangement = LinearSwitch(arrangement, N)
+    print("LinearSwitch: ", calcArrangement(optimizedArrangement)[0], optimizedArrangement, calcArrangement(optimizedArrangement))
+
+def testLinearSwitchInfluenceList(input = input1Table, N = 10):
+    from Algorithms.LinearSwitch import LinearSwitch
+    from Algorithms.InfluenceListGreedy import influenceListGreedy
+    arrangement = influenceListGreedy(input)
+    optimizedArrangement = LinearSwitch(arrangement, N)
+    print("LinearSwitch: ", calcArrangement(optimizedArrangement)[0], optimizedArrangement, calcArrangement(optimizedArrangement))
+
+def testRandomSwitch(input = input1Table):
     from Algorithms.DefaultPlacement import defaultPlacement
     arrangement = defaultPlacement(input)
-    print("Before optimization:")
-    printer.printArrangementWithValues(arrangement)
-    from Algorithms.RandomSwitch import postOptimize
-    postOptimize(arrangement)
-    print("After optimization:")
-    printer.printArrangementWithValues(arrangement)
+    from Algorithms.RandomSwitch import randomSwitch
+    randomArrangement = randomSwitch(arrangement)
+    print("RepeatedRandom: ", calcArrangement(randomArrangement)[0], randomArrangement, calcArrangement(randomArrangement))
 
 def testCustomArrangement(input = input1Table):
     arr = customArrangement(input, ["Lida", "fbuu", "D2D2", "MMMM", "D1D1", "aubu", "joho", "nsio"])
@@ -86,3 +87,15 @@ def testCustomArrangement(input = input1Table):
 
 def testcalcTheoreticalMax(input = input1Table):
     print("Theoretical max for input100people:", ValueCalc.calcTheoreticalMax(input))
+
+# testRandom(input100People)
+# testRandomGreedy(input100People)
+# testInfluenceListGreedy(input100People)
+# testDefaultPlacement(input100People)
+# testRandomSwitch(input100People)
+testRepeatedRandom(input100People)
+# testcalcTheoreticalMax(input100People)
+# testLinearSwitch(input100People, 1)
+# testLinearSwitchRandom(input100People, 1)
+# testLinearSwitchInfluenceList(input100People, 1)
+
