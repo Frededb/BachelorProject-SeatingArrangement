4e852510017b679b0dc08aeb2934a856e42a8433
Nicklas <nsio@itu.dk>
2026-02-06 12:59:21 +0100

    fix custom arrangement

--- Utils/bmalls.py
+++ Utils/bmalls.py
@@ -1,3 +1,4 @@
+from Utils import reader
 
 def getAllPeople(arrangement):
     seat_indices = [(ti, si) for ti, table in enumerate(arrangement) for si in range(len(table))]
@@ -7,12 +8,14 @@ def switch(arrangement, personA, personB):
     arrangement[personA[0]][personA[1]], arrangement[personB[0]][personB[1]] = arrangement[personB[0]][personB[1]], arrangement[personA[0]][personA[1]]
     return arrangement
 
-def customArrangement(input, personList):
-    inputNames = [person.name for person in input]
-    if set(inputNames) != set(personList):
+def customArrangement(arrangement, personList):
+    inputNames = [person.name for table in arrangement for person in table]
+    peopleMap = {person.name: person for table in arrangement for person in table}
+    personSet = set([person for table in personList for person in table])
+    if set(inputNames) != personSet:
         raise ValueError("Input and personList do not match")
-    input = sorted(input, key=lambda x: personList.index(x.name))
-    return input # should be converted to arrangement
+    new_arrangement = [list(map(lambda name: peopleMap[name] or reader.emptyPerson, table)) for table in personList]
+    return new_arrangement
 
     
     
