9f0a98085e351aae50039321edf66de688af9500
AugustBugge <aubu@itu.dk>
2026-02-06 09:55:32 +0100

    Make copy in random instead of changing actual input

--- Algorithms/Random.py
+++ Algorithms/Random.py
@@ -1,11 +1,13 @@
 import math, random
+from copy import deepcopy
 
 from Utils.reader import emptyPerson
 
 
 def randomArrangement(input):
-    arrangement = [[emptyPerson] * 8 for _ in range(math.ceil(len(input) / 8))]
-    random.shuffle(input)
-    for i in range(len(input)):
-        arrangement[i//8][i%8] = input[i]
+    inputCopy = deepcopy(input)
+    arrangement = [[emptyPerson] * 8 for _ in range(math.ceil(len(inputCopy) / 8))]
+    random.shuffle(inputCopy)
+    for i in range(len(inputCopy)):
+        arrangement[i//8][i%8] = inputCopy[i]
     return arrangement
