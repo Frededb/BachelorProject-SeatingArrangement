3c880ce53db709cdbdc073e317188d7924a84af5
AugustBugge <aubu@itu.dk>
2026-02-06 09:55:10 +0100

    added ability to run LinearSwitch multiple times

--- Algorithms/LinearSwitch.py
+++ Algorithms/LinearSwitch.py
@@ -2,26 +2,26 @@ from Utils.ValueCalc import calcTable
 from Utils.bmalls import switch, getAllPeople
 
 
-def LinearSwitch(arrangement):
+def LinearSwitch(arrangement, N):
     allPeople = getAllPeople(arrangement)
+    for _ in range(N):
+        for personA in allPeople:
+            for personB in allPeople:
+                if personA == personB:
+                    continue
 
-    for personA in allPeople:
-        for personB in allPeople:
-            if personA == personB:
-                continue
+                preValueTableA = calcTable(arrangement[personA[0]])[0]
+                preValueTableB = calcTable(arrangement[personB[0]])[0]
+                preValueTotal = preValueTableA + preValueTableB
 
-            preValueTableA = calcTable(arrangement[personA[0]])[0]
-            preValueTableB = calcTable(arrangement[personB[0]])[0]
-            preValueTotal = preValueTableA + preValueTableB
+                switch(arrangement, personA, personB)
+                postValueTableA = calcTable(arrangement[personA[0]])[0]
+                postValueTableB = calcTable(arrangement[personB[0]])[0]
+                postValueTotal = postValueTableA + postValueTableB
 
-            switch(arrangement, personA, personB)
-            postValueTableA = calcTable(arrangement[personA[0]])[0]
-            postValueTableB = calcTable(arrangement[personB[0]])[0]
-            postValueTotal = postValueTableA + postValueTableB
-
-            if postValueTotal < preValueTotal:
-                switch(arrangement, personA, personB)  # Switch back if no improvement
-            # else:
-            #     print(f"Switched {arrangement[personB[0]][personB[1]].name} and {arrangement[personA[0]][personA[1]].name} for improvement from {preValueTotal} to {postValueTotal}")
+                if postValueTotal < preValueTotal:
+                    switch(arrangement, personA, personB)  # Switch back if no improvement
+                # else:
+                #     print(f"Switched {arrangement[personB[0]][personB[1]].name} and {arrangement[personA[0]][personA[1]].name} for improvement from {preValueTotal} to {postValueTotal}")
 
     return arrangement
