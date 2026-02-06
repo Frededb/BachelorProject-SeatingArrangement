64bae18d2ab84fb813941d299f3e68745c1f6c0a
AugustBugge <aubu@itu.dk>
2026-02-06 09:56:09 +0100

    Changed name

--- Algorithms/RandomSwitch.py
+++ Algorithms/RandomSwitch.py
@@ -4,12 +4,12 @@ from Utils.bmalls import switch, getAllPeople
 
 # tablearrangement = [[8 * person] * x]
 
-def postOptimize(arrangement):
+def randomSwitch(arrangement, N=1000):
     seat_indices = getAllPeople(arrangement)
     if len(seat_indices) < 2:
         raise ValueError("Need at least two seats to choose distinct people.")
     current_score, _, _ = calcArrangement(arrangement)
-    for i in range(100):
+    for i in range(N):
         personA, personB = random.sample(seat_indices, 2)
         switch(arrangement, personA, personB)
         new_score, _, _ = calcArrangement(arrangement)
