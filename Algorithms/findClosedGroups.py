4008fed2971b5eef82db77a7725f29fe52bbe238
fbuu <fbuu@itu.dk>
2026-02-17 14:15:10 +0100

    almost done

--- Algorithms/findClosedGroups.py
+++ Algorithms/findClosedGroups.py
@@ -1,7 +1,17 @@
+from Utils.bmalls import getPersonByName
 
 
 def findClosedGroups(input):
-    for person in input:
-        print("HEJ")
-        for prefence in person.prefences:
-            print(prefence)
+    for person1 in input:
+        goal = person1.preferences + [person1.name]
+        for prefence in person1.preferences:
+            person2 = getPersonByName(prefence, input)
+            if not listContainsTheSame(goal, person2.preferences + [person2.name]):
+                break
+        else:
+            print("Closed group found: ", goal)
+
+
+def listContainsTheSame(list1, list2):
+    return set(list1) == set(list2)
+
