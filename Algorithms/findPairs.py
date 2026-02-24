0383dd4e34398814fcb336d964c3238dedf0a6aa
fbuu <fbuu@itu.dk>
2026-02-24 12:49:39 +0100

    Switch Pair, might not work tho

---
+++ Algorithms/findPairs.py
@@ -0,0 +1,12 @@
+from Utils.bmalls import getPersonByName
+
+
+def findPairs(input):
+    inputCopy = input.copy()
+    pairs = set()
+    for person1 in inputCopy:
+        for prefence in person1.preferences:
+            person2 = getPersonByName(prefence, inputCopy)
+            if prefence in person1.preferences and person1.name in person2.preferences:
+                pairs.add(frozenset({person1, person2}))
+    return pairs
