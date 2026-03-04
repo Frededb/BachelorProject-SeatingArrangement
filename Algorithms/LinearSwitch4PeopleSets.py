ad55a2eeca85cfd9883af8356efb11cded01f310
fbuu <fbuu@itu.dk>
2026-03-04 10:08:31 +0100

    Fixed for empties in sets

--- Algorithms/LinearSwitch4PeopleSets.py
+++ Algorithms/LinearSwitch4PeopleSets.py
@@ -7,29 +7,27 @@ from Utils.bmalls import getAllPeople, switch4People, switch4PeopleBack
 
 def LinearSwitch4PeopleSets(arrangement):
     allPeople = getAllPeople(arrangement)
-    #generate all combinations of 4 people
-    combinations = itertools.combinations(allPeople, 4)
+    #generate all permutations of 4 people
+    permutations = itertools.permutations(allPeople, 4)
     count = 0
-    for combination in combinations:
-        permutations = itertools.permutations(combination)
-        for permutation in permutations:
+    for permutation in permutations:
 
-            # python
-            # Replace the per-person table reads with a unique set of affected table indices
-            affected_tables = {p[0] for p in permutation}  # unique table indices affected by this 4-person swap
+        # python
+        # Replace the per-person table reads with a unique set of affected table indices
+        affected_tables = {p[0] for p in permutation}  # unique table indices affected by this 4-person swap
 
-            preValueTotal = sum(calcTable(arrangement[idx])[0] for idx in affected_tables)
+        preValueTotal = sum(calcTable(arrangement[idx])[0] for idx in affected_tables)
 
-            switch4People(arrangement, permutation[0], permutation[1], permutation[2], permutation[3])
+        switch4People(arrangement, permutation[0], permutation[1], permutation[2], permutation[3])
 
-            postValueTotal = sum(calcTable(arrangement[idx])[0] for idx in affected_tables)
+        postValueTotal = sum(calcTable(arrangement[idx])[0] for idx in affected_tables)
 
-            if postValueTotal < preValueTotal:
-                switch4PeopleBack(arrangement, permutation[0], permutation[1], permutation[2], permutation[3])
+        if postValueTotal < preValueTotal:
+            switch4PeopleBack(arrangement, permutation[0], permutation[1], permutation[2], permutation[3])
 
         count += 1
-        if count % 100000 == 0:
+        if count % 1000000 == 0:
             count = 0
-            print(f"Finished checking combinations for set: {combination}")
+            print(f"Finished checking combinations for set: {permutation}")
 
     return arrangement
