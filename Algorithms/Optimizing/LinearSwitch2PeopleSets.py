1ddfd528a62217fcbfd03d263162b5ea6b2c274b
Nicklas <nsio@itu.dk>
2026-03-27 14:56:17 +0100

    post merge

--- Algorithms/Optimizing/LinearSwitch2PeopleSets.py
+++ Algorithms/Optimizing/LinearSwitch2PeopleSets.py
@@ -18,3 +18,25 @@ def LinearSwitch2PeopleSets(arrangement):
         if postValueTotal < preValueTotal:
             switch(arrangement, personA, personB)  # Switch back if no improvementt))
     return arrangement
+
+def LinearSwitch2PeopleRandom(arrangement):
+    allPeople = getAllPeople(arrangement)
+    for _ in range(int(len(allPeople)**2)):
+        personA = random.choice(allPeople)
+        personB = random.choice(allPeople)
+        while personB == personA:
+            personB = random.choice(allPeople)
+
+        preValueTableA = calcTable(arrangement[personA[0]])[0]
+        preValueTableB = calcTable(arrangement[personB[0]])[0]
+        preValueTotal = preValueTableA + preValueTableB
+
+        switch(arrangement, personA, personB)
+
+        postValueTableA = calcTable(arrangement[personA[0]])[0]
+        postValueTableB = calcTable(arrangement[personB[0]])[0]
+        postValueTotal = postValueTableA + postValueTableB
+
+        if postValueTotal < preValueTotal:
+            switch(arrangement, personA, personB)  # Switch back if no improvement
+    return arrangement
