424f63f9eec4ca1ce646ac5ad9cfe5c7baefff6d
Nicklas <nsio@itu.dk>
2026-03-27 14:35:14 +0100

    anealing and loggin

--- Algorithms/Anealing.py
+++ Algorithms/Anealing.py
@@ -6,9 +6,12 @@ from Utils.ValueCalc import calcTable, calcArrangement
 from Utils.bmalls import getAllPeople, switch4People, switch4PeopleBack, switch3People, switch3PeopleBack, switch
 
 
-def AnealTwoPeople(arrangement, k=1000):
+def AnealTwoPeople(arrangement, k=1000, seed=None):
+    if seed is not None:
+        random.seed(seed)
     allPeople = getAllPeople(arrangement)
     #generate all combinations of 2 people
+    percents = [[0, 0] for _ in range(10)]
     preValueTotal = calcArrangement(arrangement)[0]
     for i in range(k):
         personA = random.choice(allPeople)
@@ -20,11 +23,19 @@ def AnealTwoPeople(arrangement, k=1000):
 
         postValueTotal = calcArrangement(arrangement)[0]
 
-        T = max(0.01, min(1, 1 - i / k))*100
+        #linear
+        # T = max(0.01, min(1, 1 - i / k))*120
+        #exponential
+        T = max(0.01, min(1, math.exp(-5 * i / k)))*120
 
-        P = random.random() < math.exp(-(postValueTotal - preValueTotal) / T)
+        diff = postValueTotal - preValueTotal
+        P = random.random() < math.exp((diff) / T)
+        if postValueTotal < preValueTotal:
+            percents[i*10//k][0] += P
         if postValueTotal >= preValueTotal or P:
             preValueTotal = postValueTotal
         else:
             switch(arrangement, personA, personB)
+        percents[i*10//k][1] += 1
+    print("AnealTwoPeople: " + "".join(["\n" + str(percent[0]/percent[1]*100) + "%" for percent in percents]))
     return arrangement
