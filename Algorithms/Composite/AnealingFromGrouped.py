5fefbe7d4fb8624f51429c01884b0cccf3c7ec9e
fbuu <fbuu@itu.dk>
2026-05-12 16:14:13 +0200

    removed wrappers

--- Algorithms/Composite/AnealingFromGrouped.py
+++ Algorithms/Composite/AnealingFromGrouped.py
@@ -1,21 +1,7 @@
-import time
-
 from Algorithms.Build.FluentGroupsFill import fluentGroupsFill
 from Algorithms.Optimizing.Anealing import annealing
 
 def annealingFromGrouped(input, emptyArrangement, max_seconds=None, score_tracker=None):
     arrangement, _ = fluentGroupsFill(input, emptyArrangement)
 
-    if max_seconds is None:
-        return annealing(arrangement, max_seconds=None, score_tracker=score_tracker)
-
-    start_time = time.perf_counter()
-
-    while True:
-        remaining_seconds = max_seconds - (time.perf_counter() - start_time)
-        if remaining_seconds <= 0:
-            break
-
-        arrangement = annealing(arrangement, max_seconds=remaining_seconds, score_tracker=score_tracker)
-
-    return arrangement
+    return annealing(arrangement, max_seconds=max_seconds, score_tracker=score_tracker)
--- Algorithms/Composite/AnealingFromRandom.py
+++ Algorithms/Composite/AnealingFromRandom.py
@@ -1,5 +1,3 @@
-import time
-
 from Algorithms.Build.RandomPlacement import randomPlacement
 from Algorithms.Optimizing.Anealing import annealing
 
@@ -7,17 +5,5 @@ from Algorithms.Optimizing.Anealing import annealing
 def annealingFromRandom(input, emptyArrangement, max_seconds=None, score_tracker=None):
     arrangement = randomPlacement(input, emptyArrangement)
 
-    if max_seconds is None:
-        return annealing(arrangement, max_seconds=None, score_tracker=score_tracker)
-
-    start_time = time.perf_counter()
-
-    while True:
-        remaining_seconds = max_seconds - (time.perf_counter() - start_time)
-        if remaining_seconds <= 0:
-            break
-
-        arrangement = annealing(arrangement, max_seconds=remaining_seconds, score_tracker=score_tracker)
-
-    return arrangement
+    return annealing(arrangement, max_seconds=max_seconds, score_tracker=score_tracker)
 
